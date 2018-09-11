fc2df740da86ebb2ce7f282db30da5c40fab638a
ouyang <550445318@qq.com>
2018-09-11 23:25:26 +0800

    three commit

--- app/src/main/java/android/coolweather/a55044/coolweather/WeatherActivity.java
+++ app/src/main/java/android/coolweather/a55044/coolweather/WeatherActivity.java
@@ -5,15 +5,21 @@ import android.coolweather.a55044.coolweather.gson.Forecast;
 import android.coolweather.a55044.coolweather.gson.Weather;
 import android.coolweather.a55044.coolweather.util.HttpUtil;
 import android.coolweather.a55044.coolweather.util.Utility;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.io.IOException;
 
@@ -44,11 +50,21 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
 
     private TextView sportText;
+
+    private ImageView bindPicImag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView=getWindow().getDecorView();
+
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN|
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         //初始化控件
+        bindPicImag=(ImageView) findViewById(R.id.bing_pig_img);
         weatherLayout=(ScrollView)findViewById(R.id.weather_layout);
         titleCity=(TextView)findViewById(R.id.title_city);
         titleUpdateTime=(TextView)findViewById(R.id.title_update_time);
@@ -61,6 +77,14 @@ public class WeatherActivity extends AppCompatActivity {
         sportText=(TextView)findViewById(R.id.sport_text);
         SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString =prefs.getString("weather",null);
+        String bingPic=prefs.getString("bing_pic",null);
+        if(bingPic!=null)
+        {
+            Glide.with(this).load(bingPic).into(bindPicImag);
+        }else
+        {
+            loadBingPic();
+        }
         if(weatherString!=null)
         {
             //有缓存时直接解析天气数据
@@ -73,6 +97,32 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
     }
+
+    private void loadBingPic() {
+        String requesBindPic="http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requesBindPic,new Callback(){
+            @Override
+            public void onResponse(Call call,Response response)throws IOException {
+
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bindPicImag);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
     /**
      * 根据天气id请求天气信息
      */
@@ -90,6 +140,7 @@ public class WeatherActivity extends AppCompatActivity {
 
                     }
                 });
+                loadBingPic();
             }
 
             @Override
